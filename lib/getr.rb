646a02c6ea9f813492f13011bab494ab60982064
nonnax <ironald@gmail.com>
2022-05-26 17:02:29 +0800

    update views

--- lib/getr.rb
+++ lib/getr.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 # Id$ nonnax 2022-05-09 01:37:27 +0800
 require_relative 'getr/base'
-require_relative 'getr/erbview'
+require_relative 'getr/getrv'
 require_relative 'getr/cache'
 require_relative 'getr/paginate'
--- lib/getr0.rb
+++ lib/getr0.rb
@@ -2,6 +2,6 @@
 # Id$ nonnax 2022-05-09 01:37:27 +0800
 # getr clone except that get, post, etc., methods returns their final values
 require_relative 'getr/base0'
-require_relative 'getr/erbview'
+require_relative 'getr/getr0v'
 require_relative 'getr/cache'
 require_relative 'getr/paginate'
